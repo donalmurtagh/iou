5261e38fd880adb93e79799adca5402a6b36b33e
Donal Murtagh <domurtag@yahoo.co.uk>
2013-09-29 15:30:35 +0100

    simplified initialisation of log4j

--- src/main/java/iou/gui/AppLauncher.java
+++ src/main/java/iou/gui/AppLauncher.java
@@ -7,15 +7,10 @@ import org.apache.log4j.PropertyConfigurator;
 import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.util.Enumeration;
-import java.util.Properties;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
 
 public class AppLauncher {
-
-    private static final String LOG_FILE_PATH = "iou.gui.resources.log4j";
 
     private static final Logger LOGGER = Logger.getLogger(AppLauncher.class);
 
@@ -26,18 +21,8 @@ public class AppLauncher {
      */
     public static void main(String[] args) throws FileNotFoundException, MalformedURLException {
 
-        // A very long-winded way of loading a log4j properties file from the classpath
-        ResourceBundle log4jRB = PropertyResourceBundle.getBundle(LOG_FILE_PATH);
-        Enumeration<String> keys = log4jRB.getKeys();
-        Properties log4jProps = new Properties();
-
-        while (keys.hasMoreElements()) {
-            String propKey = keys.nextElement();
-            String propValue = log4jRB.getString(propKey);
-            log4jProps.put(propKey, propValue);
-        }
-
-        PropertyConfigurator.configure(log4jProps);
+        InputStream log4jConfig = AppLauncher.class.getResourceAsStream("/log4j.properties");
+        PropertyConfigurator.configure(log4jConfig);
         LOGGER.debug("Launching login window");
 
         // Load the application's icon
