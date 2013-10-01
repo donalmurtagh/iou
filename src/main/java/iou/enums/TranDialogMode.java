ad15bdc8894fbbedc70dcbcc6927d0434516f833
Donal Murtagh <domurtag@yahoo.co.uk>
2013-10-01 12:49:17 +0100

    reformatted

--- src/main/java/iou/enums/TranDialogMode.java
+++ src/main/java/iou/enums/TranDialogMode.java
@@ -1,8 +1,10 @@
 package iou.enums;
 
 public enum TranDialogMode {
-    ADD_PAYMENT("Add Payment"), UPDATE_PAYMENT("Update Payment"), ADD_EXPENSE("Add Expense"), UPDATE_EXPENSE(
-            "Update Expense");
+    ADD_PAYMENT("Add Payment"),
+    UPDATE_PAYMENT("Update Payment"),
+    ADD_EXPENSE("Add Expense"),
+    UPDATE_EXPENSE("Update Expense");
 
     private String title;
 
