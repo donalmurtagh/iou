5bdd9e264b8e964df91f71939033f19140b6af63
domurtag <domurtag@yahoo.co.uk>
2014-11-18 00:15:07 +0000

    removed unused code

--- src/main/java/iou/enums/TransactionType.java
+++ src/main/java/iou/enums/TransactionType.java
@@ -1,16 +1,18 @@
 package iou.enums;
 
 public enum TransactionType {
+
     EXPENSE {
         @Override
         public String toString() {
             return "EXP";
         }
     },
+
     PAYMENT {
         @Override
         public String toString() {
             return "PMT";
         }
-    };
+    }
 }
