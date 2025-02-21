af24e800bd497475f4a985f33e3f8f4e33c020b9
Dónal Murtagh <domurtag@yahoo.co.uk>
2025-02-21 01:09:43 +0000

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 create table if not exists transaction (
-  id			      bigint not null auto_increment,
-  type			    enum('PMT', 'EXP') not null,
+  id            bigint not null auto_increment,
+  type          enum('PMT', 'EXP') not null,
   tran_date     date,
-  description	  varchar(255),
-  ann_paid 	    decimal(6,2),
-  bob_paid 	    decimal(6,2),
+  description   varchar(255),
+  ann_paid      decimal(6,2),
+  bob_paid      decimal(6,2),
   archived      tinyint(1) default 0,
   modified_by   varchar(20),
-  modified_at	  timestamp not null default current_timestamp,
+  modified_at   timestamp not null default current_timestamp,
   primary key(id),
   index type_index (type)
 );
